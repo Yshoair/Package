42b21cc5fa94b59a0b5cc689716d15f752425931
Yusuf Shoair PC <yshoair@gmail.com>
2021-06-25 03:19:46 +0200

    feat: Add BranchAndBoundPackStrategy for feature enhancement

---
+++ src/main/java/com/mobiquity/service/BranchAndBoundPackStrategy.java
@@ -0,0 +1,11 @@
+package com.mobiquity.service;
+
+import com.mobiquity.contract.IPackStrategy;
+import com.mobiquity.model.Package;
+
+public class BranchAndBoundPackStrategy implements IPackStrategy {
+    @Override
+    public Package pack(Package p) {
+        return null;
+    }
+}
